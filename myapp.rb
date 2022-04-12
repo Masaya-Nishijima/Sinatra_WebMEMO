29ca33d0e2a3eb9b58ac15c99bb8fb022e5222ad
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-04-12 15:35:52 +0900

    メモ一覧機能を記述

--- myapp.rb
+++ myapp.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # myapp.rb
 require 'sinatra'
 require 'sinatra/reloader'
@@ -6,12 +8,16 @@ require 'sinatra/reloader'
 #   'This is WEB MEMO app'
 #   '<h1>aaaa</h1>'
 # end
+
 get '/' do # メモ一覧の表示
-  redirect to('/index.html')
+  "
+  既存のメモリストです。<br />
+  #{make_memo_list}
+  "
+  # redirect to('/index.html')
 end
 
-get '/:memo_name' do # メモを表示
-  read_memo(params['memo_name'])
+get '/:memo_name' do # メモを表示\
   "
   参照したメモは#{params['memo_name']}です。内容は以下の通りです。<br />
   #{read_memo(params['memo_name'])} <br />
@@ -20,17 +26,32 @@ get '/:memo_name' do # メモを表示
 end
 
 post '/:memo_name' do # メモを作成
-  if File.exist?('memo_data/' + params[:memo_name])
+  if File.exist?("memo_data/#{params[:memo_name]}")
     '同名のメモがあります。タイトルを変えてください。'
   else
     'メモを作成しました'
   end
 end
 
+def make_memo_list
+  memo_name = Dir.glob('*', base: "#{Dir.getwd}/memo_data")
+  list = ''
+  memo_name.size.times do |time|
+    list += make_a_tag(memo_name[time])
+  end
+  list
+end
 
+# メモの名前からhtmlのaタグリンクを作成するメソッド
+def make_a_tag(memo_name)
+  "<a href=\"#{memo_name}\">#{memo_name}<br />"
+end
+
+# メモの中身を取得するメソッド
 def read_memo(memo_title)
-  return '指定されたメモがありません' if !File.exist?('memo_data/' + params[:memo_name])
-  file = File.new("memo_data/" + memo_title, "r")
+  return '指定されたメモがありません' unless File.exist?("memo_data/#{params[:memo_name]}")
+
+  file = File.new("memo_data/#{memo_title}", 'r')
   memo_body = file.read
   file.close
   memo_body
