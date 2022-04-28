19f9d86ea7334353520c00f41b2dc5a69a33a0ac
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-04-28 15:43:46 +0900

    リファクタリング
    4/28のレビューで、比較的少量の修正となる箇所を纏めて修正

--- myapp.rb
+++ myapp.rb
@@ -4,6 +4,8 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+DANGEROUS_STRING = '/.<>'
+
 get '/' do
   redirect to('/memo')
 end
@@ -27,7 +29,7 @@ get '/memo/new_memo' do # メモの作成フォームを表示
 end
 
 post '/memo' do # メモを作成
-  params[:memo_name].delete!('/.<>')
+  params[:memo_name].delete!(DANGEROUS_STRING)
   unique_name = generate_unique_name(params[:memo_name])
   file = File.new("memo_data/#{unique_name}", 'w')
   file.write(params[:memo_body])
@@ -36,7 +38,7 @@ post '/memo' do # メモを作成
 end
 
 delete '/memo/:memo_name' do # メモの削除メソッド
-  params[:memo_name].delete!('/.<>')
+  params[:memo_name].delete!(DANGEROUS_STRING)
   File.delete("memo_data/#{params['memo_name']}")
   redirect to('/memo')
 end
@@ -46,8 +48,8 @@ get '/memo/:memo_name/editor' do # メモの編集ページ
 end
 
 patch '/memo/:memo_name' do # メモの編集を実行
-  params[:memo_name].delete!('/.<>')
-  params[:new_memo_name].delete!('/.<>')
+  params[:memo_name].delete!(DANGEROUS_STRING)
+  params[:new_memo_name].delete!(DANGEROUS_STRING)
   unique_new_name = generate_unique_name(params[:new_memo_name])
   File.rename("#{Dir.getwd}/memo_data/#{params['memo_name']}", "#{Dir.getwd}/memo_data/#{unique_new_name}") if params['memo_name'] != params[:new_memo_name]
   file = File.new("memo_data/#{params[:new_memo_name]}", 'w+')
@@ -57,7 +59,7 @@ patch '/memo/:memo_name' do # メモの編集を実行
 end
 
 get '/memo/:memo_name' do # メモを表示
-  redirect(not_found) if read_memo(params['memo_name']) == '指定されたメモがありません'
+  redirect(not_found) if exists_memo?(params['memo_name'])
   erb :memo
 end
 
@@ -90,6 +92,11 @@ def read_memo(memo_name)
 
   h(File.open("memo_data/#{memo_name}", 'r', &:read))
 end
+
+def exists_memo?(memo_name)
+  read_memo(params['memo_name']) == '指定されたメモがありません'
+end
+
 
 def generate_unique_name(memo_name)
   return memo_name unless File.exist?("memo_data/#{memo_name}")
