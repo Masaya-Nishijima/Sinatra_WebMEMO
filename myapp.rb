67f6bdffb5ea75852158703fa55f40b770dbcfdd
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-05-18 13:34:18 +0900

    SQLインジェクション対策

--- myapp.rb
+++ myapp.rb
@@ -7,7 +7,7 @@ require 'pg'
 
 DATABASE = PG.connect(dbname: 'sinatra_web_app')
 
-DANGEROUS_STRING = '/.<>'
+# DANGEROUS_STRING = '/.<>'
 
 get '/' do
   redirect to('/memo')
@@ -32,15 +32,15 @@ get '/memo/new_memo' do # メモの作成フォームを表示
 end
 
 post '/memo' do # メモを作成
-  params[:memo_name].delete!(DANGEROUS_STRING)
+  # params[:memo_name].delete!(DANGEROUS_STRING)
   unique_name = generate_unique_name(params[:memo_name])
-  DATABASE.exec("INSERT INTO memo values ('#{unique_name}', '#{params[:memo_body]}')")
+  DATABASE.exec_params("INSERT INTO memo values ($1, $2)", [unique_name, params[:memo_body]])
   redirect to('/memo')
 end
 
 delete '/memo/:memo_name' do # メモの削除メソッド
-  params[:memo_name].delete!(DANGEROUS_STRING)
-  DATABASE.exec("DELETE FROM memo WHERE memo_name = '#{params[:memo_name]}'")
+  # params[:memo_name].delete!(DANGEROUS_STRING)
+  DATABASE.exec_params("DELETE FROM memo WHERE memo_name = $1", [params[:memo_name]])
   redirect to('/memo')
 end
 
@@ -49,16 +49,16 @@ get '/memo/:memo_name/editor' do # メモの編集ページ
 end
 
 patch '/memo/:memo_name' do # メモの編集を実行
-  params[:memo_name].delete!(DANGEROUS_STRING)
-  params[:new_memo_name].delete!(DANGEROUS_STRING)
-  params[:memo_body].delete!(DANGEROUS_STRING)
+  # params[:memo_name].delete!(DANGEROUS_STRING)
+  # params[:new_memo_name].delete!(DANGEROUS_STRING)
+  # params[:memo_body].delete!(DANGEROUS_STRING)
   unique_new_name = generate_unique_name(params[:new_memo_name])
-  DATABASE.exec("UPDATE memo SET memo_name = '#{unique_new_name}', memo_body = '#{params[:memo_body]}' WHERE memo_name = '#{params[:memo_name]}'")
+  DATABASE.exec_params("UPDATE memo SET memo_name = $1, memo_body = $2 WHERE memo_name = $3", [unique_new_name, params[:memo_body], params[:memo_name]])
   redirect to('/memo')
 end
 
 get '/memo/:memo_name' do # メモを表示
-  memo_data = DATABASE.exec("SELECT * FROM memo WHERE memo_name = '#{params[:memo_name]}'").values[0]
+  memo_data = DATABASE.exec_params("SELECT * FROM memo WHERE memo_name = $1", [params[:memo_name]]).values[0]
   redirect(not_found) if memo_data.nil?
   @memo = { memo_name: memo_data[0], memo_body: memo_data[1] }
 
