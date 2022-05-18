7e1ff828c27622b519dc3d6202afdeb9754ad2e2
Masaya-Nishijima <Masaya-Nishijima@users.noreply.github.com>
2022-05-18 15:29:30 +0900

    Rubocop通過

--- myapp.rb
+++ myapp.rb
@@ -35,18 +35,18 @@ end
 post '/memo' do # メモを作成
   params[:memo_name].delete!(DANGEROUS_STRING)
   unique_name = generate_unique_name(params[:memo_name])
-  DATABASE.exec_params("INSERT INTO memo values ($1, $2)", [unique_name, params[:memo_body]])
+  DATABASE.exec_params('INSERT INTO memo values ($1, $2)', [unique_name, params[:memo_body]])
   redirect to('/memo')
 end
 
 delete '/memo/:memo_name' do # メモの削除メソッド
   params[:memo_name].delete!(DANGEROUS_STRING)
-  DATABASE.exec_params("DELETE FROM memo WHERE memo_name = $1", [params[:memo_name]])
+  DATABASE.exec_params('DELETE FROM memo WHERE memo_name = $1', [params[:memo_name]])
   redirect to('/memo')
 end
 
 get '/memo/:memo_name/editor' do # メモの編集ページ
-  @memo_body = DATABASE.exec_params("SELECT memo_body FROM memo WHERE memo_name = $1", [params[:memo_name]])
+  @memo_body = DATABASE.exec_params('SELECT memo_body FROM memo WHERE memo_name = $1', [params[:memo_name]])
   erb :editor_memo
 end
 
@@ -55,17 +55,17 @@ patch '/memo/:memo_name' do # メモの編集を実行
   params[:new_memo_name].delete!(DANGEROUS_STRING)
   # params[:memo_body].delete!(DANGEROUS_STRING)
   unique_new_name =
-  unless params[:new_memo_name] == params[:memo_name] # 編集元のメモと同名の場合に連番を振られないようにする。
-    generate_unique_name(params[:new_memo_name])
-  else
-    params[:new_memo_name]
-  end
-  DATABASE.exec_params("UPDATE memo SET memo_name = $1, memo_body = $2 WHERE memo_name = $3", [unique_new_name, params[:memo_body], params[:memo_name]])
+    if params[:new_memo_name] == params[:memo_name]
+      params[:new_memo_name]
+    else # 編集元のメモと同名の場合に連番を振られないようにする。
+      generate_unique_name(params[:new_memo_name])
+    end
+  DATABASE.exec_params('UPDATE memo SET memo_name = $1, memo_body = $2 WHERE memo_name = $3', [unique_new_name, params[:memo_body], params[:memo_name]])
   redirect to('/memo')
 end
 
 get '/memo/:memo_name' do # メモを表示
-  memo_data = DATABASE.exec_params("SELECT * FROM memo WHERE memo_name = $1", [params[:memo_name]]).values[0]
+  memo_data = DATABASE.exec_params('SELECT * FROM memo WHERE memo_name = $1', [params[:memo_name]]).values[0]
   redirect(not_found) if memo_data.nil?
   @memo = { memo_name: memo_data[0], memo_body: memo_data[1] }
 
@@ -75,7 +75,7 @@ end
 #############################
 
 def generate_unique_name(memo_name)
-  already_memo_names = DATABASE.exec_params('SELECT memo_name FROM memo WHERE memo_name LIKE $1', [memo_name + '%']).values.flatten
+  already_memo_names = DATABASE.exec_params('SELECT memo_name FROM memo WHERE memo_name LIKE $1', ["#{memo_name}%"]).values.flatten
   return memo_name if already_memo_names.find { |already_memo_name| already_memo_name == memo_name }.nil?
 
   i = 2
